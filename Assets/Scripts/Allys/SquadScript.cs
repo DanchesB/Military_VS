f0ef40fdd721798685411eb841bd6f9fdd6ad5da
AlexanderAvdeev993 <123899359+AlexanderAvdeev993@users.noreply.github.com>
2024-03-15 19:07:19 +0300

    Revert "path4"
    This reverts commit 01b0f52eecd397887bb9da13b58d0d4ca54ef408.

--- Assets/Scripts/Allys/SquadScript.cs
+++ Assets/Scripts/Allys/SquadScript.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class SquadScript : MonoBehaviour
@@ -29,45 +30,12 @@ public class SquadScript : MonoBehaviour
         PlayerTransform = PlayerController.Instance.PlayerTransform;
         offset = PlayerTransform.TransformDirection(Vector3.back) * -_distanceBehindPlayer;
 
-        _pointsGameObjects = new List<GameObject>();               
+        _pointsGameObjects = new List<GameObject>();
+
+       
+         SpawnAlly(_currentAlly);
+        
     }
-
-    private void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            AddAlly();
-        }       
-    }
-
-    public void AddAlly()
-    {     
-        if(_currentAlly <= _maxPoints)
-        {
-            _currentAlly++;
-            SpawnAlly(1);
-        }
-        else
-            Debug.Log("MaxAlly");
-    }
-
-   /* public void RemoveAlly()
-    {    
-        if (_pointsGameObjects.Count > 0)
-        {
-            _pointsGameObjects[_pointsGameObjects.Count - 1].SetActive(false);
-                         
-            _pointsGameObjects.RemoveAt(_pointsGameObjects.Count - 1);
-
-            
-            _currentAlly--;
-        }
-        else
-        {
-            Debug.Log("Отряд пуст. Нечего удалять.");
-        }
-    }*/                                                                 // после прописки смерти союзника дописать
-
 
     private void SpawnAlly(int currentAlly)
     {
