64640b2678de1b65ebba822061ad381175e5a151
Leonid Petrov <71147031+F4ITH-Power@users.noreply.github.com>
2024-03-15 12:59:19 +0300

    Add M16

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
+    internal GameObject weaponHolder;
 
     private CountdownTimerUI countdownTimer;
     private Slider progressSlider;
@@ -12,8 +13,15 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
    {
+<<<<<<< Updated upstream
         var playerPrefab = Instantiate(_player, new Vector3(0.0f, 0, 0), Quaternion.identity);
+=======
+        var playerPrefab = Instantiate(_player, new Vector3(0.0f, 0, 0), Quaternion.identity);      
+        playerPrefab.AddComponent<Player>();
+>>>>>>> Stashed changes
 
+        weaponHolder = Finder.FindDeepChild(playerPrefab.transform, "WeaponHolder");
+        
         countdownTimer = FindObjectOfType<CountdownTimerUI>();
         progressSlider = FindObjectOfType<Slider>();
 
